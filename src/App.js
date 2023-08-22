6e5d5a93f72c2b03910dde4664d594682c2eb589
Qaisar <kayser.abbas@gmail.com>
2023-08-22 11:01:22 +0500

    Modify file for functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home.js';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import LocalStorage from './components/LocalStorage';
+import AddTask from './components/AddTask';
+import TaskList from './components/TaskList';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const savedTasks = LocalStorage.get('tasks');
+    setTasks(savedTasks);
+  }, []);
+
+  const handleAddTask = (taskText) => {
+    const newTask = { text: taskText };
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+    LocalStorage.set('tasks', updatedTasks);
+  };
+
+  const handleUpdateTask = (taskToUpdate, newText) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task === taskToUpdate) {
+        return { ...task, text: newText };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+    LocalStorage.set('tasks', updatedTasks);
+  };
+
+  const handleDeleteTask = (taskToDelete) => {
+    const updatedTasks = tasks.filter((task) => task !== taskToDelete);
+    setTasks(updatedTasks);
+    LocalStorage.set('tasks', updatedTasks);
+  };
+
   return (
-    <>
-      <nav className="nav-list">
-        <h1 className="title">Math Magician</h1>
-        <Link className="nav" to="/">
-          Home
-        </Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+    <div>
+      <h1>Todo List App</h1>
+      <AddTask onAdd={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onUpdate={handleUpdateTask}
+        onDelete={handleDeleteTask}
+      />
+    </div>
   );
-}
+};
 
 export default App;
